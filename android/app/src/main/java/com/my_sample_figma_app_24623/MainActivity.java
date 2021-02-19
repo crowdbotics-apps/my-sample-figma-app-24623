2208a78a834db40d0890d20ff683f0c7a6d4e865
Crowdbotics <team@crowdbotics.com>
2021-02-19 19:41:16 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/my_sample_figma_app_24623/MainActivity.java
@@ -0,0 +1,15 @@
+package com.my_sample_figma_app_24623;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "my_sample_figma_app_24623";
+    }
+}
---
+++ ios/my_sample_figma_app_24623Tests/my_sample_figma_app_24623Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface my_sample_figma_app_24623 : XCTestCase
+
+@end
+
+@implementation my_sample_figma_app_24623
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
